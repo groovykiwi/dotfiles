8ffd8a8ebb256f074ba9354be843d1a4d5a9c627
groovykiwi <nathanhoulamy@gmail.com>
2020-05-30 15:24:19 +0300

    misc updates

--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for entry in "etc/lightdm"/*
-do
-	sudo rm -rf "/$entry" && echo "/$entry"
-	sudo ln -s "$HOME/dotfiles/$entry" "/$entry" && echo -e "Created symlink with $entry \n"
-done
